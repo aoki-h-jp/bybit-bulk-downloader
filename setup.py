46ef4687265ce897704501964d33407353c1909b
aoki-h-jp <113173839+aoki-h-jp@users.noreply.github.com>
2023-09-05 20:22:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="bybit-bulk-downloader",
     version="1.1.0",
-    description=" Python library to efficiently and concurrently download historical data files from Binance. Supports all asset types (spot, USDT-M, COIN-M, options) and all data frequencies.",
+    description=" Python library to efficiently and concurrently download historical data files from bybit. Supports all asset types (spot, USDT-M, COIN-M, options) and all data frequencies.",
     install_requires=["requests", "rich", "pytest", "beautifulsoup4", "pandas", "pybit"],
     author="aoki-h-jp",
     author_email="aoki.hirotaka.biz@gmail.com",

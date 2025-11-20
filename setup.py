bcc236ff22aaa38795785542d0929934ce350eec
aoki-h-jp <hirotaka.aoki@solopreneurs.tech>
2025-11-20 22:00:42 +0900

    Bump version to 1.2.2 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="bybit-bulk-downloader",
-    version="1.2.1",
+    version="1.2.2",
     description=" Python library to efficiently and concurrently download historical data files from Bybit. Supports all asset types (spot, USDT-M, COIN-M, options) and all data frequencies.",
     install_requires=["requests", "rich", "pytest", "beautifulsoup4", "pybit"],
     author="aoki-h-jp",

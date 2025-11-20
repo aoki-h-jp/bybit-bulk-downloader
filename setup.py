de50caf6bbfd5a42d39093a21106edd027ccbafb
aoki-h-jp <hirotaka.aoki@solopreneurs.tech>
2025-11-20 21:25:31 +0900

    Update setup.py for version 1.2.1: Corrected description and author email, added pybit to requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="bybit-bulk-downloader",
-    version="1.2.0",
-    description=" Python library to efficiently and concurrently download historical data files from Binance. Supports all asset types (spot, USDT-M, COIN-M, options) and all data frequencies.",
-    install_requires=["requests", "rich", "pytest", "beautifulsoup4"],
+    version="1.2.1",
+    description=" Python library to efficiently and concurrently download historical data files from Bybit. Supports all asset types (spot, USDT-M, COIN-M, options) and all data frequencies.",
+    install_requires=["requests", "rich", "pytest", "beautifulsoup4", "pybit"],
     author="aoki-h-jp",
-    author_email="aoki.hirotaka.biz@gmail.com",
+    author_email="hirotaka.aoki@solopreneurs.tech",
     license="MIT",
     packages=["bybit_bulk_downloader"],
 )
